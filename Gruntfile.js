52a5b01074eb2205f208ef2c8df85e28aef27194
Przemysław Fałowski <przemkow92@gmail.com>
2015-11-06 02:18:23 +0000

    add sass, improve layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,8 @@ module.exports = function(grunt) {
         },
         "watch": {
             "build": {
-                files: ["app/**/*.js", "app/**/*.html", "app/**/*.css" ],
+                files: ["app/**/*.js", "app/**/*.html", "app/**/*.scss" ],
+                tasks: ['sass'],
                 options: {
                     livereload: true,
                 }
@@ -28,16 +29,27 @@ module.exports = function(grunt) {
                     logConcurrentOutput: true
                 }
             }
+        },
+        sass: {
+          options: {
+            sourceMap: true
+          },
+          dist: {
+            files: {
+              'app/common/app.css': 'app/common/scss/app.scss',
+              'app/common/sidebar.css': 'app/common/scss/sidebar.scss'
+            }
+          }
         }
     });
 
     //
     // Register modules
     //
-    grunt.loadNpmTasks("grunt-contrib-connect");
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-concurrent");
+    require('load-grunt-tasks')(grunt);
 
     // Default task(s).
-    grunt.registerTask("serve", ["concurrent:build"]);
+    grunt.registerTask("serve", ["sass","concurrent:build"]);
+    grunt.registerTask("compile", ["sass"]);
+
 };
